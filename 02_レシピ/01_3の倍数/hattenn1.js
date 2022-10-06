cb9621dc8bcac84736d32c1caa56aa004db5bc52
kanna1204 <masegiganko@gmail.comgit config --global user.name kanna1204>
2022-10-06 22:54:41 +0900

    [3の倍数]発展１です。

---
+++ 02_レシピ/01_3の倍数/hattenn1.js
@@ -0,0 +1,13 @@
+const genkiFunction = function (number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 5 === 0) {
+      console.log("Bizz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else {
+      console.log(n)
+    }
+  }
+}
